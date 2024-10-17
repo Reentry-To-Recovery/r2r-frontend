b78a1543bfb058c057bc2913ccea88cc4d2eabb5
baojiao9799 <41097137+baojiao9799@users.noreply.github.com>
2024-10-17 12:05:20 -0400

    move courses to admin dashboard

--- src/pages/Dashboard/Dashboard.js
+++ src/pages/Dashboard/Dashboard.js
@@ -1,5 +1,5 @@
 import { useAuth0 } from "@auth0/auth0-react";
-import { useUserRole } from "../../hooks/useUserRole";
+import { useUserRole, UserRole } from "../../hooks/useUserRole";
 import StudentDash from "../../components/Dashboards/StudentDash";
 import AdminDash from "../../components/Dashboards/AdminDash";
 
@@ -8,9 +8,9 @@ export default function Dashboard() {
   const { userRole } = useUserRole();
 
   return (
-    <div className="webpage dashboard flex align">
+    <div className={`webpage flex align dashboard`}>
       <div className="inner-webpage">
-        {isAuthenticated && userRole === "admin" ? (
+        {isAuthenticated && userRole === UserRole.Admin ? (
           <AdminDash />
         ) : (
           <StudentDash />
