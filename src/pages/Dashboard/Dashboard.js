f47578f0d2e95e6363e57cdd296567c773282748
baojiao9799 <41097137+baojiao9799@users.noreply.github.com>
2024-10-17 14:54:21 -0400

    update styling

--- src/pages/Dashboard/Dashboard.js
+++ src/pages/Dashboard/Dashboard.js
@@ -1,5 +1,5 @@
 import { useAuth0 } from "@auth0/auth0-react";
-import { useUserRole, UserRole } from "../../hooks/useUserRole";
+import { UserRole, useUserRole } from "../../hooks/useUserRole";
 import StudentDash from "../../components/Dashboards/StudentDash";
 import AdminDash from "../../components/Dashboards/AdminDash";
 
@@ -8,7 +8,7 @@ export default function Dashboard() {
   const { userRole } = useUserRole();
 
   return (
-    <div className={`webpage flex align dashboard`}>
+    <div className={`webpage flex align ${isAuthenticated && userRole === UserRole.Admin ? '' : 'dashboard'}`}>
       <div className="inner-webpage">
         {isAuthenticated && userRole === UserRole.Admin ? (
           <AdminDash />
